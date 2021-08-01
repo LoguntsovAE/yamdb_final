09fd434749698cbfa1d8e3973fec11577b9d73f2
LoguntsovAE <loguntsovae@yandex.ru>
2021-08-01 21:43:42 +0500

    Настрйока в джанге для разворачивания проекта_5

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECRET_KEY = os.getenv('SECRET_KEY_DJANGO')
 SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '178.154.222.154',
